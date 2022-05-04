d7457dc1c3edd39627102933f5baa63cd939d99b
Arnab Bhakta <arnabbhakta23@gmail.com>
2022-05-04 21:56:17 +0530

    add Ex-09

---
+++ WebApp01/Pages/HtmlHelperEx09.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp01.Pages
+{
+	public class HtmlHelperEx09Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost(string language)
+        {
+            ViewData["Message"] = $"Selected language {language}";
+        }
+    }
+}
