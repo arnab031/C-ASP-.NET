4231f976a17b5be07ad8fbb98eb94971a5aadbdf
arnab031 <arnabbhakta23@gmail.com>
2022-05-09 21:21:05 +0530

    Add Example 7

---
+++ WebApp01/Pages/AdoEx07.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp01.Pages
+{
+	public class AdoEx07Model : PageModel
+    {
+        string cs = string.Empty;
+        string query = string.Empty;
+        SqlConnection cn = null;
+        SqlCommand cmd = null;
+        SqlDataReader dr = null;
+
+        public AdoEx07Model()
+        {
+           cs = "server=localhost;Database=SalesDB;User=sa;Password=Arnab@123";
+            cn = new SqlConnection(cs);
+            cn.Open();
+        }
+        public void OnGet()
+        {
+            query = "spGetAllProducts";
+            cmd = new SqlCommand(query, cn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            dr = cmd.ExecuteReader();
+            List<Product> Products = new List<Product>();
+            while (dr.Read())
+            {
+                Product p1 = new Product();
+                p1.Id = Convert.ToInt32(dr[0]);
+                p1.ProductName = dr[1].ToString();
+                p1.Model = dr[2].ToString();
+                p1.Price = Convert.ToInt32(dr[3]);
+                Products.Add(p1);
+            }
+            cn.Dispose();
+            dr.Close();
+            ViewData["Products"] = Products;
+        }
+    }
+}
---
+++ WebApp01/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WebApp01
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string ProductName { get; set; }
+		public string Model { get; set; }
+		public int Price { get; set; }
+		public Product()
+		{
+			
+		}
+	}
+}
+
