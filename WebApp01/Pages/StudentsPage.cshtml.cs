e51771335977e354e2fa8b7c9594062f45da6576
arnab031 <51916075+arnab031@users.noreply.github.com>
2022-05-12 21:53:20 +0530

    Dependency Injection through list of students

---
+++ WebApp01/Pages/StudentsPage.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp01.Pages
+{
+	public class StudentsPageModelModel : PageModel
+    {
+        StudentContext context = null;
+        public StudentsPageModelModel()
+        {
+            //context = new StudentContext(new DotNetStudentRepository());
+            context = new StudentContext(new JavaStudentRepository());
+        }
+        public void OnGet()
+        {
+            ViewData["Students"] = context.GetAllStudents();
+            ViewData["Category"] = context.GetCategory();
+        }
+    }
+}
---
+++ WebApp01/StudentModel1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace WebApp01
+{
+	public class StudentModel1
+	{
+		public int Id { get; set; }
+		public string SName { get; set; }
+		public string Course { get; set; }
+		public int Fee { get; set; }
+
+
+	}
+	public interface IStudentRepository
+    {
+		List<StudentModel1> GetAllStudents();
+		string Category();
+    }
+	class DotNetStudentRepository : IStudentRepository
+    {
+		public string Category()
+        {
+			return ".NET";
+        }
+		public List<StudentModel1> GetAllStudents()
+        {
+			return new List<StudentModel1>
+			{
+				new StudentModel1{Id=1001,SName="Dev",Course="MVC.NET",Fee=25000},
+				new StudentModel1{Id=1002,SName="Dhanush",Course="ASP.NET Core",Fee=22000},
+				new StudentModel1{Id=1003,SName="Devender",Course="EF Core",Fee=21000},
+				new StudentModel1{Id=1001,SName="Dayman",Course=".NET",Fee=20000}
+			};
+        }
+    }
+	class JavaStudentRepository : IStudentRepository
+	{
+		public string Category()
+		{
+			return "Java";
+		}
+		public List<StudentModel1> GetAllStudents()
+		{
+			return new List<StudentModel1>
+			{
+				new StudentModel1{Id=2001,SName="Jaya",Course="Spring",Fee=25000},
+				new StudentModel1{Id=2002,SName="Joyita",Course="Springboot",Fee=22000},
+				new StudentModel1{Id=2003,SName="Javed",Course="Core Java",Fee=21000},
+				new StudentModel1{Id=2004,SName="Jiya",Course="Java",Fee=20000}
+			};
+		}
+	}
+	public class StudentContext
+    {
+		IStudentRepository _repository;
+		public StudentContext(IStudentRepository repository)
+        {
+			this._repository = repository;
+        }
+		public List<StudentModel1> GetAllStudents()
+		{
+			return this._repository.GetAllStudents();
+		}
+		public string GetCategory()
+		{
+			return this._repository.Category();
+		}
+	}
+}
+
